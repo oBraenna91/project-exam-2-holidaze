917f4d454836d0b0f5866f510dd15bb575d064a6
oBraenna91 <95278401+oBraenna91@users.noreply.github.com>
2023-09-13 12:30:25 +0200

    Update index.js

--- src/hooks/useSearch/index.js
+++ src/hooks/useSearch/index.js
@@ -1,3 +1,25 @@
+// import { useEffect, useState } from 'react';
+
+// function useSearch(venues) {
+//   const [searchValue, setSearchValue] = useState('');
+//   const [searchResults, setSearchResults] = useState([]);
+
+//   function handleSearchValueChange(event) {
+//     setSearchValue(event.target.value);
+//   }
+
+//   useEffect(() => {
+//     const filteredVenues = venues.filter((venue) =>
+//       venue.name.toLowerCase().includes(searchValue.toLowerCase())
+//     );
+//     setSearchResults(filteredVenues);
+//   }, [searchValue, venues]);
+
+//   return { searchValue, searchResults, handleSearchValueChange };
+// }
+
+// export default useSearch;
+
 import { useEffect, useState } from 'react';
 
 function useSearch(venues) {
@@ -9,9 +31,20 @@ function useSearch(venues) {
   }
 
   useEffect(() => {
-    const filteredVenues = venues.filter((venue) =>
-      venue.name.toLowerCase().includes(searchValue.toLowerCase())
-    );
+    const filteredVenues = venues.filter((venue) => {
+      const lowerSearchValue = searchValue.toLowerCase();
+      const lowerVenueName = venue.name.toLowerCase();
+      const location = venue.location;
+
+      // Check if the search value is included in the lowercase venue name
+      // or if it's included in the lowercase city or country of the location
+      return (
+        lowerVenueName.includes(lowerSearchValue) ||
+        (location.city.toLowerCase().includes(lowerSearchValue) ||
+          location.country.toLowerCase().includes(lowerSearchValue))
+      );
+    });
+
     setSearchResults(filteredVenues);
   }, [searchValue, venues]);
 
