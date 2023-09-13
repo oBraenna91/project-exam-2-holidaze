74a97247a6b7ea8a98e7aaf705ec388934648b22
oBraenna91 <95278401+oBraenna91@users.noreply.github.com>
2023-09-13 13:09:49 +0200

    Update index.jsx

--- src/components/mini-card/index.jsx
+++ src/components/mini-card/index.jsx
@@ -8,7 +8,7 @@ export function MiniCard({ venue }) {
   return (
     <div className="mini-card mb-3">
         <div className="mx-1 mini-card-text-container">
-            <Link to={`venue/${venue.id}`} className="text-primary">
+            <Link to={`venue/${id}`} className="text-primary">
                 <p className="mini-card-text">{name.toUpperCase()}</p>
             </Link>
         </div>
@@ -16,7 +16,7 @@ export function MiniCard({ venue }) {
             <div className="col-1 me-2">
               <BlueLocationIcon key="location-icon"/>
             </div>
-            <p className="mini-card-location">{location.city}, {location.country}</p>
+            <p className="mini-location">{location.city}, {location.country}</p>
         </div>
     </div>
   );
