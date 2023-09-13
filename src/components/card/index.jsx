095bd140cb871ed2b6a2207bbb321d18197223ca
oBraenna91 <95278401+oBraenna91@users.noreply.github.com>
2023-09-13 13:09:46 +0200

    Update index.jsx

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -2,16 +2,26 @@ import CustomButton from '../button';
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
 import logoImage from '../../logo/dark_logo.png';
+import RenderIcons from '../render-icons';
+import BlueLocationIcon from '../icons/location';
 
 
 export function VenueCard({ venue }) {
-    const mediaUrl = venue.media.length > 0 ? venue.media[0] : logoImage;
+  const {name, media, id, meta, location} = venue;
+  const mediaUrl = media.length > 0 ? media[0] : logoImage;
   return (
     <Card className="my-3 shadow-lg venue-card" style={{ width: '18rem' }}>
-      <Card.Img className="card-image" variant="top" src={mediaUrl} alt={venue.name}/>
+      <Card.Img className="card-image" variant="top" src={mediaUrl} alt={name}/>
       <Card.Body>
-        <Card.Title className="my-3 h2">{venue.name.toUpperCase()}</Card.Title>
-        <Link to={`venue/${venue.id}`}><CustomButton label="Book" /></Link>
+        <Card.Title className="my-3 h2">{name.toUpperCase()}</Card.Title>
+        <div className="mini-location-container mb-4 d-flex m-auto justify-content-center">
+            <div className="col-1 me-2">
+              <BlueLocationIcon key="location-icon"/>
+            </div>
+            <p className="mini-location">{location.city}, {location.country}</p>
+        </div>
+        <RenderIcons meta={meta} />
+        <Link to={`venue/${id}`}><CustomButton className="mt-4" label="More info" /></Link>
       </Card.Body>
     </Card>
   );
