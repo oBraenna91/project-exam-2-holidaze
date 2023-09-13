b82c78acd5bdf9089415dfa30f78c66ab5e505b4
oBraenna91 <95278401+oBraenna91@users.noreply.github.com>
2023-09-13 12:30:31 +0200

    Create index.jsx

---
+++ src/components/icons/location/index.jsx
@@ -0,0 +1,9 @@
+import Image from 'react-bootstrap/Image';
+import locationIcon from '../../../icons/blue_location.png';
+
+export function blueLocationIcon() {
+    
+    return  <Image src={locationIcon} fluid />;
+    
+}
+export default blueLocationIcon;
