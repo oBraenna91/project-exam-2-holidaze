7260bd1fa86528f3ed5befbef0e5b3cce0137e9a
oBraenna91 <95278401+oBraenna91@users.noreply.github.com>
2023-09-13 13:09:53 +0200

    Update index.js

--- src/hooks/useSearch/index.js
+++ src/hooks/useSearch/index.js
@@ -1,25 +1,3 @@
-// import { useEffect, useState } from 'react';
-
-// function useSearch(venues) {
-//   const [searchValue, setSearchValue] = useState('');
-//   const [searchResults, setSearchResults] = useState([]);
-
-//   function handleSearchValueChange(event) {
-//     setSearchValue(event.target.value);
-//   }
-
-//   useEffect(() => {
-//     const filteredVenues = venues.filter((venue) =>
-//       venue.name.toLowerCase().includes(searchValue.toLowerCase())
-//     );
-//     setSearchResults(filteredVenues);
-//   }, [searchValue, venues]);
-
-//   return { searchValue, searchResults, handleSearchValueChange };
-// }
-
-// export default useSearch;
-
 import { useEffect, useState } from 'react';
 
 function useSearch(venues) {
