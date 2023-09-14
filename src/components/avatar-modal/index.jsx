7540609b9ec226a92708a453b636afe13ebbef90
oBraenna91 <95278401+oBraenna91@users.noreply.github.com>
2023-09-14 20:02:56 +0200

    Update index.jsx

--- src/components/avatar-modal/index.jsx
+++ src/components/avatar-modal/index.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import { updateAvatar }from '../../hooks/useUpdateAvatar';
 import ConfirmationModal from '../confirmation-modal';
