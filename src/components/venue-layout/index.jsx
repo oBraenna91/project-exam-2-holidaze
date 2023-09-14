cde93e58e78515128b47ae9feb3389696e24efb2
oBraenna91 <95278401+oBraenna91@users.noreply.github.com>
2023-09-14 19:55:48 +0200

    Update index.jsx

--- src/components/venue-layout/index.jsx
+++ src/components/venue-layout/index.jsx
@@ -16,7 +16,9 @@ export function VenueLayout({ venue }) {
     return (
         <div>
             <h1 className="my-5">{name}</h1>
-            <Image src={mediaUrl} alt={name} fluid />
+            <div className="col-12 col-md-6 m-auto">
+                <Image src={mediaUrl} alt={name} fluid />
+            </div>
             <h2 className="mt-5">Location:</h2>
             <p className="venue-p">{location.address}</p>
             <p className="venue-p">{location.city} , {location.country}</p>
@@ -25,25 +27,25 @@ export function VenueLayout({ venue }) {
             <h3 className="">Price: {price} $</h3>
             <h3 className="">Guests: max {maxGuests}</h3>
             <div className="d-flex my-5 flex-column justify-content-center align-items-center">
-                <div className="col-8 my-3 d-flex align-items-center justify-content-between">
+                <div className="col-8 col-md-3 my-3 d-flex align-items-center justify-content-between">
                     <div className="col-3">
                         {meta.wifi ? <WifiIcon /> : <NoWifiIcon />}
                     </div>
                     <p className="my-0">WiFi included: {meta.wifi ? 'Yes' : 'No'}</p>
                 </div>
-                <div className="col-8 my-3 d-flex align-items-center justify-content-between">
+                <div className="col-8 col-md-3 my-3 d-flex align-items-center justify-content-between">
                     <div className="col-3">
                         {meta.pets ? <PetsIcon /> : <NoPetsIcon />}
                     </div>
                     <p className="my-0">Pets allowed: {meta.pets ? 'Yes' : 'No'}</p>
                 </div>
-                <div className="col-8 my-3 d-flex align-items-center justify-content-between">
+                <div className="col-8 col-md-3 my-3 d-flex align-items-center justify-content-between">
                     <div className="col-3">
                         {meta.breakfast ? <BreakfastIcon /> : <NoBreakfastIcon />}
                     </div>
                     <p className="my-0">Breakast included: {meta.breakfast ? 'Yes' : 'No'}</p>
                 </div>
-                <div className="col-8 my-3 d-flex align-items-center justify-content-between">
+                <div className="col-8 col-md-3 my-3 d-flex align-items-center justify-content-between">
                     <div className="col-3">
                         {meta.parking ? <ParkingIcon /> : <NoParkingIcon />}
                     </div>
