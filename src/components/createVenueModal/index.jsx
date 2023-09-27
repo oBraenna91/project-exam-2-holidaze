3906ba06f4d98be9545be2ef23eb110bd4a4dfaa
oBraenna91 <95278401+oBraenna91@users.noreply.github.com>
2023-09-27 15:09:45 +0200

    Update index.jsx

--- src/components/createVenueModal/index.jsx
+++ src/components/createVenueModal/index.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-// import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import CustomButton from '../button';
 import CreateVenueForm from '../create-venue-form';
