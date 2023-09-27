1130ed514b9b0689fd017b852ce01102a2c3cad4
oBraenna91 <95278401+oBraenna91@users.noreply.github.com>
2023-09-27 15:10:01 +0200

    Delete index.jsx

--- src/components/search-icon/index.jsx
+++
@@ -1,9 +0,0 @@
-import Image from 'react-bootstrap/Image';
-import searchIcon from '../../icons/blue_search.png';
-
-function SearchIcon() {
-    
-    return  <Image src={searchIcon} fluid />;
-    
-}
-export default SearchIcon;
