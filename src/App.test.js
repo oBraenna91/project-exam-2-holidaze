aa9655e71cf0ba8a476161a448093154d5bba1c0
oBraenna91 <95278401+oBraenna91@users.noreply.github.com>
2023-09-27 11:58:36 +0200

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,10 @@
 //   const linkElement = screen.getByText(/learn react/i);
 //   expect(linkElement).toBeInTheDocument();
 // });
+
+
+// describe('Demo tests', () => {
+//     test('1 is equal to 1', () => {
+//       expect(1).toBe(1);
+//     });
+//   });
