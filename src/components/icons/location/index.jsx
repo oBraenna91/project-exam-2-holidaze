8af9f4d8b78c6d2c024185ff632d2db7333df992
oBraenna91 <95278401+oBraenna91@users.noreply.github.com>
2023-09-27 12:37:12 +0200

    Update index.jsx

--- src/components/icons/location/index.jsx
+++ src/components/icons/location/index.jsx
@@ -3,8 +3,15 @@ import Image from 'react-bootstrap/Image';
 import locationIcon from '../../../icons/blue_location.png';
 
 export function BlueLocationIcon() {
+
+    const imageStyle = {
+        maxWidth: '20px',
+        maxHeight: '20px',
+        width: 'auto',
+        height: 'auto',
+    };
     
-    return  <Image src={locationIcon} fluid />;
+    return  <Image src={locationIcon} style={imageStyle} />; //fluid />;
     
 }
 export default BlueLocationIcon;
