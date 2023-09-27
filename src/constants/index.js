7ae7f7ddb551374ec2659855583363f301be38cd
oBraenna91 <95278401+oBraenna91@users.noreply.github.com>
2023-09-27 15:10:10 +0200

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,8 +2,6 @@ export const API_BASE_URL = 'https://api.noroff.dev/api/v1/holidaze/';
 
 export const VENUES_URL = `${API_BASE_URL}venues`;
 
-// ?_bookings=true 
-
 export const BOOKINGS_URL = `${API_BASE_URL}bookings`;
 
 export const REGISTER_URL = `${API_BASE_URL}auth/register`;
